c2cbd3dc48406584a9290af655d6b2e4fcc4a235
jacinto jacking <jgamboita@gmail.com>
2022-08-22 16:15:52 -0500

    cambindo nombre APITest

--- evaluacion-docente-api/src/test/java/co/gov/educacionbogota/evaluaciondocenteapi/ApiApplicationTests.java
+++ evaluacion-docente-api/src/test/java/co/gov/educacionbogota/evaluaciondocenteapi/ApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EvaluacionDocenteApiApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
