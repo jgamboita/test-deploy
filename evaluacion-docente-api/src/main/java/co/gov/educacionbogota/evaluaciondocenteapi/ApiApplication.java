904a8f5379dcb3afc204af16218217b1afeb3fcf
jacinto jacking <jgamboita@gmail.com>
2022-09-05 15:59:21 -0500

    ajustes

--- evaluacion-docente-api/src/main/java/co/gov/educacionbogota/evaluaciondocenteapi/ApiApplication.java
+++ evaluacion-docente-api/src/main/java/co/gov/educacionbogota/evaluaciondocenteapi/ApiApplication.java
@@ -3,10 +3,16 @@ package co.gov.educacionbogota.evaluaciondocenteapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.gov.educacionbogota.evaluaciondocenteapi.infra.in.HealthAdapter;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		log.info("Aplicación saludable e iniciada!!" );
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
--- evaluacion-docente-api/src/main/java/co/gov/educacionbogota/evaluaciondocenteapi/infra/in/HealthAdapter.java
+++ evaluacion-docente-api/src/main/java/co/gov/educacionbogota/evaluaciondocenteapi/infra/in/HealthAdapter.java
@@ -2,6 +2,11 @@ package co.gov.educacionbogota.evaluaciondocenteapi.infra.in;
 
 import co.gov.educacionbogota.evaluaciondocenteapi.application.in.HealthPort;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +25,17 @@ public class HealthAdapter  implements HealthPort<String> {
         log.info("Aplicación saludable!!" + entity);
         return "Aplicación saludable!!" + entity;
 
+    }
+    
+    private GroupedOpenApi prepareGroup(String pGroup, String pPath) {
+        return GroupedOpenApi.builder()
+            .group(pGroup)
+            .pathsToMatch(pPath)
+            .build();
+    }
+
+    @Bean
+    public List<GroupedOpenApi> getApis() {
+        return List.of(this.prepareGroup("evaluacion-docente-api", "/evaluacion-docente-api/**"));
     }
 }
